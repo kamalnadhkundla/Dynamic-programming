259e3c59d870b545befb390056560a56d94ca0f1
KAMALNADH KUNDLA <60436100+kamalnadhkundla@users.noreply.github.com>
2021-12-04 11:03:31 +0530

    Update 11_longestcommonsubsequence(lc1143).java

--- 11_longestcommonsubsequence(lc1143).java
+++ 11_longestcommonsubsequence(lc1143).java
@@ -124,3 +124,29 @@ class Solution {
             
         }
 }
+
+///////////////////////////////////////////////////top down dp///////////////////////////////
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+      int len1=text1.length();
+        int len2=text2.length();
+        
+        int[][] dp = new int[len1+1][len2+1];
+        
+        for(int i =1;i<dp.length;i++){
+            for(int j  =1;j<dp[i].length;j++){
+               
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                     dp[i][j]=1+dp[i-1][j-1];
+                    System.out.println(dp[i][j]);
+                }
+                   
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[len1][len2];
+        
+    }
+}
