d1d0af15b9a8b9b79831765a8bdba71d33d14ae9
KAMALNADH KUNDLA <60436100+kamalnadhkundla@users.noreply.github.com>
2021-12-05 12:19:22 +0530

    Update 13_shortestcommonsupersequence.java

--- 13_shortestcommonsupersequence.java
+++ 13_shortestcommonsupersequence.java
@@ -35,3 +35,53 @@ class Solution
     }
 }
 LEETCODE(HARD ->1092):
+// we need to know how to print lcs string dont think blindly in lcs when we add plus one in dp table means a common character we may even get the same common character again by again adding 
+plus one so there is another approach to solve comming from bottom to top apporach 
+public class Solution{
+    
+
+public String shortestCommonSupersequence(String s1, String s2) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        for(int i = 0; i <= l1; i++){
+            for(int j = 0; j <= l2; j++){
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if(s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        int i = l1, j = l2;
+        while(i > 0 && j > 0){
+            if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                result.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            }else{
+                if(dp[i - 1][j] > dp[i][j - 1]) {
+                    result.append(s1.charAt(i - 1));
+                    i--;
+                }
+                else {
+                    result.append(s2.charAt(j - 1));
+                    j--;
+                }
+            }
+        }
+        while(i > 0){
+            result.append(s1.charAt(i - 1));
+            i--;
+        }
+        while(j > 0){
+            result.append(s2.charAt(j - 1));
+            j--;
+        }
+        return result.reverse().toString();
+    }
+}
